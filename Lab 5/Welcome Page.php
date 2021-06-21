d6eb1a7d361820e41f279a9c20aad6fc7c7d7f00
nishat <nishattasnim671@gmail.com>
2021-06-21 23:23:10 +0600

    Project database and form

---
+++ Lab 5/Welcome Page.php
@@ -0,0 +1,39 @@
+
+<?php 
+?> 
+<html>
+	<head></head>
+	<body>
+		<form>
+		
+		<fieldset>
+		
+			<table >
+			
+			<b><center>Welcome to Learn Panda<br>				
+			<tr>
+					<td rowspan= "3"><center>Course :</td>																	
+					<td>
+					    
+						<input type="submit" name="submit" value="Learn HTML"> <br>
+						 <input type="submit" name="submit" value="Learn PHP"> <br>
+						 <input type="submit" name="submit" value="Learn JavaScript"style="margin-right: 250px"> <br>
+						 <input type="submit" name="submit" value="Learn C#"> <br>
+						<input type="submit" name="submit" value="Learn Java"> <br>
+						<input type="submit" name="submit" value="Learn Database">
+						
+					</td>
+					<td rowspan= "5" ><input type="submit" name="submit" value="Dash Board Of Log-IN" style="margin-right: 250px"  > 
+					<input type="button" value="About US"> </td>	
+			
+			</tr>
+			
+			
+			</table>
+			
+			
+			
+		</fieldset>
+		</form>
+	</body>
+</html>
